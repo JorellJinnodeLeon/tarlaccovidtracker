b07cd0b07102dfc4455fe7060bbe121166a68147
Jorell de Leon <jorelljinnodeleon@gmail.com>
2020-04-09 18:14:12 +0800

    trial

--- index.php
+++ index.php
@@ -1,1 +1,74 @@
-<?php include_once("home.html"); ?>
+<html>
+  <head></head>
+  <body>
+    <!--
+    BEFORE RUNNING:
+    ---------------
+    1. If not already done, enable the Google Sheets API
+       and check the quota for your project at
+       https://console.developers.google.com/apis/api/sheets
+    2. Get access keys for your application. See
+       https://developers.google.com/api-client-library/javascript/start/start-js#get-access-keys-for-your-application
+    3. For additional information on authentication, see
+       https://developers.google.com/sheets/api/quickstart/js#step_2_set_up_the_sample
+    -->
+    <script>
+    function makeApiCall() {
+      var params = {
+        spreadsheetId: '1KqMF4eaJengJ_8Dl2dewEL8LpC3WB0uoek9oF0DYkfY', 
+        range: 'Sheet01!D4:D4',  
+      };
+
+      var request = gapi.client.sheets.spreadsheets.get(params);
+      request.then(function(response) {
+        // TODO: Change code below to process the `response` object:
+        console.log(response.result);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
+
+    function initClient() {
+      var API_KEY = 'AIzaSyALkVM1V0UqYlu7DIISYJ9mAxzn4_9ZOms';  // TODO: Update placeholder with desired API key.
+
+      var CLIENT_ID = '942300299579-tsgtqo7r0oit397rec3f9vsojercnhhm.apps.googleusercontent.com';  // TODO: Update placeholder with desired client ID.
+
+      var SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+      
+      gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+      }).then(function() {
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
+    }
+
+    function handleClientLoad() {
+      gapi.load('client:auth2', initClient);
+    }
+
+    function updateSignInStatus(isSignedIn) {
+      if (isSignedIn) {
+        makeApiCall();
+      }
+    }
+
+    function handleSignInClick(event) {
+      gapi.auth2.getAuthInstance().signIn();
+    }
+
+    function handleSignOutClick(event) {
+      gapi.auth2.getAuthInstance().signOut();
+    }
+    </script>
+    <script async defer src="https://apis.google.com/js/api.js"
+      onload="this.onload=function(){};handleClientLoad()"
+      onreadystatechange="if (this.readyState === 'complete') this.onload()">
+    </script>
+    <button id="signin-button" onclick="handleSignInClick()">Sign in</button>
+    <button id="signout-button" onclick="handleSignOutClick()">Sign out</button>
+  </body>
+</html>
